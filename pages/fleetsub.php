1d8f0d756aafb090df2335ff80bdde1ef1e0d4c4
Orsokuma <magictallguy@hotmail.com>
2025-09-08 16:19:04 +0100

    Remove redundant blank ID parameters

--- pages/fleetsub.php
+++ pages/fleetsub.php
@@ -29,41 +29,40 @@ if ($user['airbucks'] < $premiumcost) {
         <br />
         <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';
 } else {
-$db->query("INSERT INTO `userairplanes` (`id`,`planeOWNER`, `planeID`, `planePASSENGER`, `planePASSENGERCURRENT`, `planeMAXFUEL`, `planeFUELCURRENT`, `planeSPEED`, `planeMAXDISTANCE`, `planeDISTANCETRAVELLED`, `planeMAXWEIGHT`, `planeCURRENTWEIGHT`, `planeCONSUMPTIONRATE`, `planeLOCATIONLAT`, `planeLOCATIONLON`, `planeACTIVE`) VALUES 
-('','$userid','$planeID','$planePASSENGER','0','$planeFUEL','0','$planeSPEED','$planeDISTANCE','0','$planeWEIGHT','0','$planeCONSUMPTIONRATE','$planeLAT','$planeLON','0')");
-$i = $db->insert_id(); 
+$db->query("INSERT INTO `userairplanes` (`planeOWNER`, `planeID`, `planePASSENGER`, `planePASSENGERCURRENT`, `planeMAXFUEL`, `planeFUELCURRENT`, `planeSPEED`, `planeMAXDISTANCE`, `planeDISTANCETRAVELLED`, `planeMAXWEIGHT`, `planeCURRENTWEIGHT`, `planeCONSUMPTIONRATE`, `planeLOCATIONLAT`, `planeLOCATIONLON`, `planeACTIVE`) VALUES 
+('$userid','$planeID','$planePASSENGER','0','$planeFUEL','0','$planeSPEED','$planeDISTANCE','0','$planeWEIGHT','0','$planeCONSUMPTIONRATE','$planeLAT','$planeLON','0')");
+$i = $db->insert_id();
 $db->query("INSERT INTO `money`(`userid`, `outin`, `amount`,`item`,`date`,`type`) VALUES ('$userid','out','$premiumcost','Purchase: Plane: <b>$i</b>',unix_timestamp(),'airbucks')");
-$db->query("UPDATE users SET airbucks=airbucks-'$premiumcost' WHERE userid=$userid");    
+$db->query("UPDATE users SET airbucks=airbucks-'$premiumcost' WHERE userid=$userid");
 echo '<p>Success</p><br />
         You have just paid <span class="airbucks">'.money_formatter($premiumcost).'</span> bucks for this plane.<br />
         <br />
-        <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';    
-}       
+        <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';
+}
 } else {
 if ($user['bucks'] < $cost) {
     echo 'You cannot afford to buy this plane<br />
         <br />
         <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';
 } else {
-$db->query("INSERT INTO `userairplanes` (`id`,`planeOWNER`, `planeID`, `planePASSENGER`, `planePASSENGERCURRENT`, `planeMAXFUEL`, `planeFUELCURRENT`, `planeSPEED`, `planeMAXDISTANCE`, `planeDISTANCETRAVELLED`, `planeMAXWEIGHT`, `planeCURRENTWEIGHT`, `planeCONSUMPTIONRATE`, `planeLOCATIONLAT`, `planeLOCATIONLON`, `planeACTIVE`) VALUES 
-('','$userid','$planeID','$planePASSENGER','0','$planeFUEL','0','$planeSPEED','$planeDISTANCE','0','$planeWEIGHT','0','$planeCONSUMPTIONRATE','$planeLAT','$planeLON','0')");
-$i = $db->insert_id(); 
+$db->query("INSERT INTO `userairplanes` (`planeOWNER`, `planeID`, `planePASSENGER`, `planePASSENGERCURRENT`, `planeMAXFUEL`, `planeFUELCURRENT`, `planeSPEED`, `planeMAXDISTANCE`, `planeDISTANCETRAVELLED`, `planeMAXWEIGHT`, `planeCURRENTWEIGHT`, `planeCONSUMPTIONRATE`, `planeLOCATIONLAT`, `planeLOCATIONLON`, `planeACTIVE`) VALUES 
+('$userid','$planeID','$planePASSENGER','0','$planeFUEL','0','$planeSPEED','$planeDISTANCE','0','$planeWEIGHT','0','$planeCONSUMPTIONRATE','$planeLAT','$planeLON','0')");
+$i = $db->insert_id();
 $db->query("INSERT INTO `money`(`userid`, `outin`, `amount`,`item`,`date`,`type`) VALUES ('$userid','out','$cost','Purchase: Plane: <b>$i</b>',unix_timestamp(),'bucks')");
-$db->query("UPDATE users SET bucks=bucks-'$cost' WHERE userid=$userid");    
+$db->query("UPDATE users SET bucks=bucks-'$cost' WHERE userid=$userid");
 echo '<p>Success</p><br />
         You have just paid <span class="bucks">'.money_formatter($cost).'</span> bucks for this plane.<br />
         <br />
-        <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';    
-}   
+        <a href="../pages/fleets.php?u='.$userid.'" target="fleets">Continue</a>';
+}
 }
 
 
 
 
 
-?>    
+?>
 
         </table>
 
       </div>
-      
