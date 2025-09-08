58dc04dbb5bcac5f3d1c9712ecd696567c697937
Orsokuma <magictallguy@hotmail.com>
2025-09-08 13:03:02 +0100

    Support EnvVars

--- required/config.php
+++ required/config.php
@@ -1,10 +1,28 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * @param string $name
+ *
+ * @return string|null
+ */
+function read_secret(string $name): ?string
+{
+    $name = strtoupper($name);
+    if ($val = getenv($name)) {
+        return trim($val);
+    }
+    if ($val = getenv($name . '_FILE')) {
+        return trim(file_get_contents($val));
+    }
+    return null;
+}
+
 $_CONFIG = [
-    'hostname'   => 'localhost',
-    'username'   => 'YOURDBUSERNAME',
-    'password'   => 'YOURDBPASSWORD',
-    'database'   => 'YOURDBDATABASE',
-    'persistent' => 0,
-    'driver'     => 'mysqli',
-    'code'       => 'blahblah',
+    'hostname' => read_secret('DB_HOST') ?? 'localhost',
+    'username' => read_secret('DB_USER') ?? 'YOURDBUSERNAME',
+    'password' => read_secret('DB_PASS') ?? 'YOURDBPASSWORD',
+    'database' => read_secret('DB_NAME') ?? 'YOURDBDATABASE',
+    'driver'   => 'mysqli',
+    'code'     => read_secret('APP_KEY') ?? 'blahblah',
 ];
